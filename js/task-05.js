26b0b60928732aef9bae6450442753e8bc693dfe
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2022-11-29 16:50:26 +0200

    5 hotfix

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,9 @@
 const nameInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 nameInput.addEventListener("input", (event) => {
-    output.textContent = null ? Anonymous : event.currentTarget.value;
+        output.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === ''){
+        output.textContent = 'Anonymous';
+    }
         
 })
